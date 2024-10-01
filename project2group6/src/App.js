c7b9c412f7c41dbe5c4d7fa1d431c42b4b4b8450
Trinity Stallworth <trinitystallworth57@gmail.com>
2024-09-30 17:31:19 -0700

    Started the work on the landing page. Did not include the necessary functionality yet.

--- project2group6/src/App.js
+++ project2group6/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Let's Make a Wishlist!</h1>
+    <div className = "loginSignup">
+    <div className ="login">
+      <h3>Login?</h3>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
+    </div>
+    <div className = "Google">
+      <h3>Google</h3>
+      <h2>You can use Google to signup or login!!!</h2>
+      <h2>Just use the button below!</h2>
+      <button>Google</button>
+    </div>
+    <div className = "signup">
+      <h3>Signup?</h3>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <input type ="secondPassowrd" placeholder="Re-enter passowrd" />
+      <button>Login</button>
+    </div>
+    </div>
     </div>
   );
 }
