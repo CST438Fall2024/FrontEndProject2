743d76f2ba2d139f643aa5dcf858afbeddfe997e
Trinity Stallworth <trinitystallworth57@gmail.com>
2024-10-15 22:48:37 -0700

    Started working on sessions. React-Key-Authentication does not work. Will try different option

--- project2group6/src/pages/App.js
+++ project2group6/src/pages/App.js
@@ -12,13 +12,13 @@ function App() {
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState(''); // Added confirmPassword state
-  const databaseUrl = '"proxy": "https://mysterious-brook-69944-1a2d962c192a.herokuapp.com"';
+  const databaseUrl = '/users/';
 
   // Fetching data on component mount
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${databaseUrl}all`);
+        const response = await axios.get(`/users/all`);
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -26,19 +26,19 @@ function App() {
     };
     fetchData();
 
-    const loadGapi = () => {
-      if (window.gapi) {
-        window.gapi.load('auth2', () => {
-          window.gapi.auth2.init({
-            client_id: '497722883096-d2i832qs7k7oamjuv62kcre3somnh9ig.apps.googleusercontent.com',
-          });
-        })
-      }
-      else {
-        console.error('gapi is not loaded');
-      }
-    };
-    loadGapi();
+    // const loadGapi = () => {
+    //   if (window.gapi) {
+    //     window.gapi.load('auth2', () => {
+    //       window.gapi.auth2.init({
+    //         client_id: '497722883096-d2i832qs7k7oamjuv62kcre3somnh9ig.apps.googleusercontent.com',
+    //       });
+    //     })
+    //   }
+    //   else {
+    //     console.error('gapi is not loaded');
+    //   }
+    // };
+    // loadGapi();
 
   }, []);
 
@@ -50,16 +50,16 @@ function App() {
         username,
         password,
       });
+      console.log(`${databaseUrl}login`);
       if (response.status === 200) {
         navigate('/list');
       } else {
         alert('Invalid credentials. Please try again.');
       }
     } catch (error) {
-      alert('Something went wrong. Please try again.');
+      alert('Cannot login. An error occured.');
     }
   };
-
   // Signup function, checks password confirmation before signing up
   const signup = async () => {
     if (password !== confirmPassword) {
@@ -72,7 +72,7 @@ function App() {
         password,
       });
       if (response.status === 200) {
-        login(); // Automatically log in after signup
+        alert("You have signed in successfully");
       }
     } catch (error) {
       alert('Something went wrong during signup.');
