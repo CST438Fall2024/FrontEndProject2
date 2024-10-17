174e3b4e97e3b9b5fb9ad01f1a424583c8438d15
Trinity Stallworth <trinitystallworth57@gmail.com>
2024-10-16 20:19:35 -0700

    Started implementing a Modal to change the user information from the Admin side

--- project2group6/src/pages/Admin.js
+++ project2group6/src/pages/Admin.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import '../css/List.css'; // uses same style
-import { useNavigate } from 'react-router-dom';
 import Layout from '../Layout';
+import { Modal, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Admin() {
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -25,35 +26,65 @@ function Admin() {
     fetchUsers();
   }, []);
 
-  // NAVIGATION
-  const navigate = useNavigate();
+  const editUsers = async (usersId) => {
+    try {
+      const response = await axios.get(`${databaseUrl}all`, {
+        params: {userId},
+      });
+      setEditingUser(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+    const responseEdit = await axios.post(`${databaseUrl}edit`);
 
-  const handleNavigation = (path) => {
-    navigate(path);
-  };
+  }
+
+  const deleteUsers = (users.id) => { }
+
+  const Popup = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+      <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input type="text" className="form-control" placeholder= {users?.username}></input>
+        </Modal.Body>
+      </Modal>
+      </>
+    )
+  }
+  const renderContent = () => {
+    if (loading) {
+      return <p>Loading Users...</p>
+    }
+    if (error) {
+      return <p>{error}</p>
+    }
+    if (users.length > 0) {
+      return users.map((user, index) => (
+        <div key={index} className="item">
+          <span>{user.username}</span>
+          <div className="button-container">
+            <button onClick={editUsers(user.id)}>Edit</button>
+            <button onClick={deleteUsers(user.id)}>Delete</button>
+          </div>
+        </div>
+      ))
+    }
+  }
 
   return (
     <div className="container">
       <Layout>
         <h1>All Users</h1>
         <div className="item-list">
-          {loading ? (
-            <p>Loading users</p>
-          ) : error ? (
-            <p>{error}</p>
-          ) : users.length > 0 ? ( // print all users
-            users.map((user, index) => (
-              <div key={index} className="item">
-                <span>{user.username}</span>
-                <div className="button-container">
-                    <button onClick={() => alert('Edit clicked!')}>Edit</button>
-                    <button onClick={() => alert('Remove clicked')}>Remove</button>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p>No users available</p>
-          )}
+          {renderContent()}
         </div>
       </Layout>
     </div>
